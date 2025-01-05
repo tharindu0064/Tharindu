639f25b8d84fc141df306a2ef8d2d8fbd4800cd1
tharindu0064 <vipkingshaluka@gmail.com>
2025-01-05 13:43:45 +0530

    Update ping.js

--- commandes/ping.js
+++ commandes/ping.js
@@ -7,7 +7,7 @@ const { default: axios } = require('axios');
 zokou({ nomCom: 'ping',
     desc: 'To check ping',
     Categorie: 'General',
-    reaction: '🚨', 
+    reaction: '🥵', 
     fromMe: 'true', 
 
        
@@ -15,7 +15,7 @@ zokou({ nomCom: 'ping',
   async (dest, zk, commandeOptions) => {
     const { ms, arg, repondre } = commandeOptions;
     const { start} = new Date().getTime()
-    return repondre('📌 *MAFI_TAIGER ♨ PING*\n ```' + 2000 + '``` *ms*') 
+    return repondre('💗*MAFI_TAIGER ♨ PING*\n ```' + 2000 + '``` *ms*') 
     const { end } = new Date().getTime()
     await zok.sendMessage('*Pong!*\n ```' + (end - start) + '``` *ms*')
   }
