1d2d1a29b2de4ace815d0b0e7c72e84b4b132ee0
tharindu0064 <vipkingshaluka@gmail.com>
2025-01-05 12:14:09 +0530

    Update fancy.js

--- commandes/fancy.js
+++ commandes/fancy.js
@@ -1,14 +1,14 @@
 const { zokou } = require("../framework/zokou");
 const fancy = require("../commandes/style");
 
-zokou({ nomCom: "fancy", categorie: "Fun", reaction: "☑️" }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "fancy", categorie: "Fun", reaction: "💗" }, async (dest, zk, commandeOptions) => {
     const { arg, repondre, prefixe } = commandeOptions;
     const id = arg[0]?.match(/\d+/)?.join('');
     const text = arg.slice(1).join(" ");
 
     try {
         if (id === undefined || text === undefined) {
-            return await repondre(`\nExample : ${prefixe}fancy 10 Dexter-Md\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('DEXTER-MD', fancy));
+            return await repondre(`\nExample : ${prefixe}fancy 10 MAFI_MD_BOT\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('DEXTER-MD', fancy));
         }
 
         const selectedStyle = fancy[parseInt(id) - 1];
